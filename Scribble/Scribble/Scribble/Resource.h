2910e161deeb710574f47d24a8ba234a7ae4f986
cash <cash924n@gmail.com>
2012-06-14 15:48:47 +0800

    Step2 phase 1 complete. Tool bar & menu bar are modified.

--- Scribble/Scribble/Scribble/Resource.h
+++ Scribble/Scribble/Scribble/Resource.h
@@ -2,18 +2,26 @@
 // Microsoft Visual C++ generated include file.
 // Used by Scribble.rc
 //
-#define IDD_ABOUTBOX				100
-#define IDP_OLE_INIT_FAILED			100
-#define IDR_MAINFRAME				128
-#define IDR_ScribbleTYPE				129
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_ScribbleTYPE                129
+#define ID_32771                        32771
+#define ID_FILE_CLEAR_ALL               32772
+#define ID_32773                        32773
+#define ID_EDIT_C                       32774
+#define ID_32775                        32775
+#define ID_PEN_THICK_OR_THIN            32776
+#define ID_PEN_WIDTHS                   32777
+#define ID_BUTTON32778                  32778
 
-// 下一個新增物件的預設值
-//
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	130
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32779
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
